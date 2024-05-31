4ed63a92df9a52997a8813d4fb5408ef3eba6c18
Andre-Santos-de-Souza <160162720+Andre-Santos-de-Souza@users.noreply.github.com>
2024-05-31 16:10:24 -0300

    Terminei a aula15.py

---
+++ aula15.py
@@ -0,0 +1,11 @@
+#A função `input()` é usada para receber entrada do usuário através do teclado. 
+nome = input("Qual o seu nome? ")
+print(f'O seu nome é {nome}')
+print(f'O seu nome é {nome=}')
+
+numero_1 = input("Digite um número: ")
+numero_2 = input('Digite outro número: ')
+
+soma = int(numero_1)+int(numero_2)
+
+print(f"A soma dos números é {soma}")
