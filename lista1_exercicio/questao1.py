7c68e684f8edaedfe3b2f81af4dce4e895a7d78b
Andre-Santos-de-Souza <160162720+Andre-Santos-de-Souza@users.noreply.github.com>
2024-06-01 14:17:34 -0300

    Fiz a questão1 da lista 1 de exercício

---
+++ lista1_exercicio/questao1.py
@@ -0,0 +1,12 @@
+largura_terreno = input("Digite a largura do terreno:  ")
+
+comprimento_terreno = input("Digite o comprimento do terreno: ")
+
+valor_metro_quadrado = input("Digite o valor do metro quadrado: ")
+
+area_terreno = float(largura_terreno) * float(comprimento_terreno)
+
+preco_terreno = float(valor_metro_quadrado) * float(area_terreno)
+
+print(f'Area do terreno = {area_terreno:.2f}')
+print(f'Preco do terreno = {preco_terreno:.2f}')
