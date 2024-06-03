662d4dca58c1ccdedcf2079ec1b6c807a517c03f
Andre-Santos-de-Souza <160162720+Andre-Santos-de-Souza@users.noreply.github.com>
2024-06-03 10:45:21 -0300

    Finalizei a aula18 e a aula19

---
+++ aula19.py
@@ -0,0 +1,18 @@
+"""
+Operadores de comparação (relacionais)
+OP......siginificado........Exemplo (True)
+>.......maior...............2 > 1
+>=......maior ou igual......2 >= 2
+<.......menor...............1 < 2
+<=......menor ou igual......2 <= 2
+==......igual...............'a' == 'a'
+!=......diferente...........'a' != 'b'
+"""
+#No terminal do powershell utilize o comando "python -i aula19.py", serve para testar o código e para sair digite "quit()".
+maior = 2 > 1
+maior_ou_igual = 2 >= 2
+menor = 1 < 2
+menor_ou_igual = 2 <= 2
+igual = 'a' == 'a'
+diferente = 'a' != 'b'
+print(maior_ou_igual)
